997d582c4a630b7a8673d71c15b1e65d03ab8903
Faluckes <FaluckesDev@protonmail.com>
2023-06-22 20:26:38 -0300

    Update

---
+++ Meu-Primeiro_projeto/src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+    public static void main (String [] args){
+        String primeiroNome = "Faluckes";
+        String segundoNome = "Munhoz";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(primeiroNome);
+        System.out.println(segundoNome);
+
+        System.out.println(nomeCompleto);
+        
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
